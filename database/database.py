80f6ddad6284bb5b90f333a3d8816f2537ad2c71
Scuffi <grawlixpvp@gmail.com>
2023-04-20 13:04:16 +0100

    Added database types and Database abstract class

--- database/database.py
+++ database/database.py
@@ -1,2 +1,28 @@
-class Database:
-    ...
+from abc import ABC, abstractmethod
+
+from .database_types import Job, RegisterJob, DeleteJob
+
+
+class Database(ABC):
+    @abstractmethod
+    def register_job(
+        workspace_id: str,
+        job_name: str,
+        tags: list[str] | None,
+        environment: str | None,
+    ) -> RegisterJob:
+        ...
+
+    @abstractmethod
+    def get_job(
+        workspace_id: str,
+        job_name: str,
+    ) -> Job:
+        ...
+
+    @abstractmethod
+    def delete_job(
+        workspace_id: str,
+        job_name: str,
+    ) -> DeleteJob:
+        ...
